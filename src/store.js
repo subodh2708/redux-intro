259357090c88f37bd2643be33d32264ccc7a564a
Subodh <subodhlst@gmail.com>
2026-01-16 13:56:58 +0530

    create action creators

--- src/store.js
+++ src/store.js
@@ -38,14 +38,39 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// console.log(store.getState());
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "buy a car" },
+// });
+// console.log(store.getState());
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
 console.log(store.getState());
-store.dispatch({ type: "account/withdraw", payload: 200 });
+store.dispatch(withdraw(200));
 console.log(store.getState());
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "buy a car" },
-});
+store.dispatch(requestLoan(1000, "buy a cheap car"));
 console.log(store.getState());
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(payLoan());
 console.log(store.getState());
