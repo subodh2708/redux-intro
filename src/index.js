55f87bbc6cd2fb541a0f56ed6b50007bbfe54321
Subodh <subodhlst@gmail.com>
2026-01-30 12:20:31 +0530

    create redux file structure with feature folder

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./store";
+import store from "./store";
+
+store.dispatch({ type: "account/deposit", payload: 450 });
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
